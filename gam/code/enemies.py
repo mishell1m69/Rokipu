062203e5382cd3d97c06e1368fb2d100ccf0a550
Clément MICHEL <159059762+mishell1m69@users.noreply.github.com>
2025-01-08 11:44:33 +0100

    Update enemies.py

--- gam/code/enemies.py
+++ gam/code/enemies.py
@@ -1,21 +1,28 @@
-"""The enemy"""
+"""
+The enemy
+"""
+
 import pygame
 from entity import Entity
 import utlis.custom_math as cm
 import constants as ct
 
+
 class Enemy(Entity):
 
-    def __init__(self,pos=[0,0],speed=1):
-        Entity.__init__(self,pos,speed)
+    def __init__(self, pos=[0, 0], speed = 1):
+        Entity.__init__(self, pos, speed)
 
+    
     def draw(self):
-        pygame.draw.rect(ct.RENDER_BUFFER, [255,0,0], pygame.Rect(self.pos[0]-8, self.pos[1]-8, 16, 16))
+        pygame.draw.rect(ct.RENDER_BUFFER, [255, 0, 0], pygame.Rect(self.pos[0] - 8, self.pos[1] - 8, 16, 16))
 
-    def movement(self,player_pos):
+    
+    def movement(self, player_pos):
         self.gotoward(player_pos)
 
-    def update(self,player_pos):
+    
+    def update(self, player_pos):
         self.entity_update()
         self.movement(player_pos)
 
