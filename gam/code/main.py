52380fefc80b6fd69b23e3680f76726a0f8c11f7
Clément MICHEL <159059762+mishell1m69@users.noreply.github.com>
2025-01-08 10:58:05 +0100

    Update main.py

--- gam/code/main.py
+++ gam/code/main.py
@@ -10,6 +10,7 @@ from enemies import Enemy
 from player import Player
 import constants as ct
 
+
 class App:
 
     def __init__(self):
@@ -21,19 +22,19 @@ class App:
         self.plist=[] #Particle list. Ugly AF, please fix
 
         #Creating basics of the game 
-        self.screen_mult=7
-        self.screen_size=(ct.GAME_DRAW_SIZE_X*self.screen_mult,ct.GAME_DRAW_SIZE_Y*self.screen_mult)
+        self.screen_mult = 7
+        self.screen_size = (ct.GAME_DRAW_SIZE_X * self.screen_mult, ct.GAME_DRAW_SIZE_Y * self.screen_mult)
         pygame.init()
         self.running = True
-        ct.RENDER_BUFFER = pygame.Surface((ct.GAME_DRAW_SIZE_X,ct.GAME_DRAW_SIZE_Y), pygame.OPENGL)
+        ct.RENDER_BUFFER = pygame.Surface((ct.GAME_DRAW_SIZE_X, ct.GAME_DRAW_SIZE_Y), pygame.OPENGL)
         self.screen = pygame.display.set_mode(self.screen_size)
 
         #Initiating game
-        self.tiles=pygame.image.load("../resources/floor_tiles.png").convert()
-        self.gamestate="Playing"
-        self.player=Player([100,100],1)
-        self.test_enemy=Enemy([200,200],0.2)
-        self.test_enemy2=Enemy([300,100],0.3)
+        self.tiles = pygame.image.load("../resources/floor_tiles.png").convert()
+        self.gamestate = "Playing"
+        self.player = Player([100,100],1)
+        self.test_enemy = Enemy([200,200],0.2)
+        self.test_enemy2 = Enemy([300,100],0.3)
 
         #Starting game loop
         self.loop()
@@ -48,20 +49,20 @@ class App:
             keys = pygame.key.get_pressed()
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_P and self.gamestate=="Playing":
-                        self.gamestate="Paused"
+                    if event.key == pygame.K_P and self.gamestate == "Playing":
+                        self.gamestate = "Paused"
                     elif event.key == pygame.K_P:
-                        self.gamestate="Playing"
+                        self.gamestate = "Playing"
                 elif event.type == pygame.QUIT:
                     self.running = False
             if keys[pygame.K_k]:
                 time.sleep(1)
                 
-            if self.gamestate=="Paused":
+            if self.gamestate == "Paused":
                 continue
             self.update()
             self.draw()
-            pygame.transform.scale_by(ct.RENDER_BUFFER,self.screen_mult,self.screen)
+            pygame.transform.scale_by(ct.RENDER_BUFFER, self.screen_mult, self.screen)
             pygame.display.update()
         pygame.quit()
 
@@ -70,13 +71,15 @@ class App:
         ct.RENDER_BUFFER.fill([0,0,0])
         for x in range(ct.GAME_DRAW_SIZE_X+16):
             for y in range(ct.GAME_DRAW_SIZE_Y):
+                
                 """
                 if (int(x/16)+int(y/12))%2==0:
                     pygame.Surface.set_at(ct.RENDER_BUFFER,(x,y),[25,25,25])
                 """
-                if x%16==0 and y%12==0 and (int(x/16)+int(y/12))%2==0:
-                    ct.RENDER_BUFFER.blit(self.tiles, (x-16, y))
-                    #pygame.Surface.set_at(ct.RENDER_BUFFER,(x,y),[100,0,0])
+                
+                if x % 16 == 0 and y % 12 == 0 and (int(x / 16) + int(y / 12)) % 2 == 0:
+                    ct.RENDER_BUFFER.blit(self.tiles, (x - 16, y))
+                    #pygame.Surface.set_at(ct.RENDER_BUFFER, (x, y), [100,0,0])
                         
         self.player.draw()
         self.test_enemy.draw()
@@ -85,7 +88,7 @@ class App:
         
 
     def update(self):
-        self.plist.append(Particle(list(self.player.pos),random.randint(100,150))) #Will be replaced by particle generator
+        self.plist.append(Particle(list(self.player.pos), random.randint(100, 150))) #Will be replaced by particle generator
         self.player.update()
         self.test_enemy.update(self.player.pos)
         self.test_enemy2.update(self.player.pos)
