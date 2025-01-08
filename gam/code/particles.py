6b23114121611fa782af0cd2d0c035ca636e771a
Clément MICHEL <159059762+mishell1m69@users.noreply.github.com>
2025-01-08 11:48:38 +0100

    Update particles.py

--- gam/code/particles.py
+++ gam/code/particles.py
@@ -1,32 +1,39 @@
 import pygame
 import random
-
 import constants as ct
+
+
 def update_particles_list(l):
     for i, particle in reversed(list(enumerate(l))):
         if particle.update():
             l.pop(i)
 
+
 class Particle:
-    """A single particle. quite useless on its own.
+    
+    """
+    A single particle. quite useless on its own.
     Should rather be used in a particle system
     """
+
+    
     def __init__(self,pos,lifespan):
-        self.col=[255,255,200]
-        self.pos=pos
-        self.fulllife=lifespan
-        self.lifespan=lifespan
-        
+        self.col = [255, 255, 200]
+        self.pos = pos
+        self.fulllife = lifespan
+        self.lifespan = lifespan
+
+    
     def update(self):
-        self.lifespan-=1
-        if self.lifespan<=0:
+        self.lifespan -= 1
+        if self.lifespan <= 0:
             return True
         #Random aaaaah shit for a fire particle
-        self.pos[1]-=random.random()*0.5*(self.fulllife/100)
-        self.pos[0]+=(random.random()-0.5)*(self.fulllife/100)
-        self.col[0]-=(255/self.fulllife)*0.4
-        self.col[1]-=(255/self.fulllife)*1.1
-        self.col[2]-=(255/self.fulllife)*2
+        self.pos[1] -= random.random()*0.5*(self.fulllife/100)
+        self.pos[0] += (random.random()-0.5)*(self.fulllife/100)
+        self.col[0] -= (255/self.fulllife)*0.4
+        self.col[1] -= (255/self.fulllife)*1.1
+        self.col[2] -= (255/self.fulllife)*2
         self.col=[min(max(self.col[0],0),255),min(max(self.col[1],0),255),min(max(self.col[2],0),255)] #makes sure the values never go over limits [0,255]
         pygame.draw.circle(ct.RENDER_BUFFER, self.col, self.pos, self.lifespan/20+0.5)
         
